ed0a8bd5d52fb091b1f2e5dde8af00fed8ab86e7
Thota Arshitha <thotaarshitha2005@gmail.com>
2024-12-23 23:50:53 +0530

    Player class

---
+++ PlayerMovement.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private bool isJumping;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (moveInput != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(moveInput), 1, 1);
+        }
+    }
+
+    private void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            isJumping = false;
+        }
+    }
+}
